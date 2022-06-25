c93a704b3171c9d64636923be0aaa1bc2fb90dfa
PAWAN KUMAR TRIPATHI <78968923+Pawan-Kumar-Tripathi@users.noreply.github.com>
2022-06-25 21:59:54 +0530

    Add files via upload

---
+++ printNaturalNum.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class printNaturalNum
+    {
+        public static void main()
+        {
+            Scanner sc = new Scanner(System.in);
+            int N = sc.nextInt();
+            for(int i = 1 ; i<=N; i++)
+            {
+                System.out.print(i);
+            }
+            
+        }
+        
+    }
